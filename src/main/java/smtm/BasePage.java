adfcb52ffdef9a30bd82b863d19bf78a8158e7f7
wrdasilva <walter.rodriguez.dasilva@gmail.com>
2025-05-25 15:26:08 -0300

    Creating the BasePage

---
+++ src/main/java/smtm/BasePage.java
@@ -0,0 +1,50 @@
+package smtm;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage {
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    protected BasePage(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofMillis(3000));
+    }
+
+    public void setup() {
+        driver.manage().window().maximize();
+    }
+
+    public void url(String url) throws InterruptedException {
+        driver.get(url);
+    }
+
+    public void close() {
+        driver.quit();
+    }
+
+    protected WebElement findElement(By locator) {
+        return driver.findElement(locator);
+    }
+
+    protected void sendText(By locator, String text) {
+        wait.until(ExpectedConditions.elementToBeClickable(locator));
+        this.findElement(locator).clear();
+        this.findElement(locator).sendKeys(text);
+    }
+
+    protected void click(By locator) {
+        wait.until(ExpectedConditions.elementToBeClickable(locator));
+        this.findElement(locator).click();
+    }
+
+    protected String getText(By locator) {
+        return this.findElement(locator).getText();
+    }
+}
